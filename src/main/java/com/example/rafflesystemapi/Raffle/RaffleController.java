6e59cb46ccccd41cbba01a02c027912433ae4cd2
Alejandro Flores <jafg84alejandro@gmail.com>
2024-11-05 13:05:17 -0600

    Se termina CRUD

--- src/main/java/com/example/rafflesystemapi/Raffle/RaffleController.java
+++ src/main/java/com/example/rafflesystemapi/Raffle/RaffleController.java
@@ -1,6 +1,7 @@
 package com.example.rafflesystemapi.Raffle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,9 +23,19 @@ public class RaffleController {
     public List<Raffle> getAllRaffles() {
         return raffleService.getAllRaffles();
     }
+
     @PostMapping
-    public void addRaffle(@RequestBody Raffle raffle) {
-        this.raffleService.newRaffle(raffle);
+    public ResponseEntity<Object> addRaffle(@RequestBody Raffle raffle) {
+        return this.raffleService.newRaffle(raffle);
     }
 
+    @PutMapping
+    public ResponseEntity<Object> updateRaffle(@RequestBody Raffle raffle) {
+        return this.raffleService.newRaffle(raffle);
+    }
+
+    @DeleteMapping(path = "{raffleId}")
+    public ResponseEntity<Object> deleteRaffle(@PathVariable("raffleId") Long id) {
+        return this.raffleService.deleteRaffle(id);
+    }
 }
--- src/main/java/com/example/rafflesystemapi/Raffle/RaffleService.java
+++ src/main/java/com/example/rafflesystemapi/Raffle/RaffleService.java
@@ -1,12 +1,21 @@
 package com.example.rafflesystemapi.Raffle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class RaffleService {
+
+    HashMap<String, Object> statusMessage;
+
     private final RaffleRepository raffleRepository;
 
     @Autowired
@@ -17,11 +26,48 @@ public class RaffleService {
         return this.raffleRepository.findAll();
     }
 
-    public void newRaffle(Raffle raffle) {
+    public ResponseEntity<Object> newRaffle(Raffle raffle) {
+        statusMessage = new HashMap<>();
         Optional<Raffle> res = raffleRepository.findRaffleByName(raffle.getName());
-        if(res.isPresent()) {
-            throw new IllegalStateException("Raffle with a same name already exists");
+        statusMessage.put("name", raffle.getName());
+
+        if(res.isPresent() && raffle.getId() == null) {
+            statusMessage.put("Error: ", true);
+            statusMessage.put("message", "Raffle already exists");
+            return new ResponseEntity<>(
+                    statusMessage,
+                    HttpStatus.CONFLICT
+            );
+        }
+        statusMessage.put("message", "Raffle created successfully");
+        if(raffle.getId() > 0) {
+            statusMessage.put("message", "Raffle successfully updated");
         }
         raffleRepository.save(raffle);
+        statusMessage.put("Data: ", raffle);
+
+        return new ResponseEntity<>(
+                statusMessage,
+                HttpStatus.CREATED
+        );
+    }
+
+    public ResponseEntity<Object> deleteRaffle(Long id) {
+        statusMessage = new HashMap<>();
+        boolean exist = this.raffleRepository.existsById(id);
+        if(!exist) {
+            statusMessage.put("Error: ", true);
+            statusMessage.put("message", "Raffle does not exists");
+            return new ResponseEntity<>(
+                    statusMessage,
+                    HttpStatus.CONFLICT
+            );
+        }
+        raffleRepository.deleteById(id);
+        statusMessage.put("message", "Raffle successfully deleted");
+        return new ResponseEntity<>(
+                statusMessage,
+                HttpStatus.ACCEPTED
+        );
     }
 }
